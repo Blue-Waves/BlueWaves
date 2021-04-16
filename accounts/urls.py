097d2c8f3cfe1766f304b0c34c3b1368f0e4c1a9
nevoh <nevogola@gmail.com>
2021-04-16 15:25:31 +0300

    Update urls.py

--- accounts/urls.py
+++ accounts/urls.py
@@ -13,10 +13,6 @@ urlpatterns = [
     path('user_profile/', views.user_profile, name='user_profile'),
     path('user_profile/edit_profile/', views.edit_profile, name='edit_profile'),
     path('change_password', views.change_password, name='change_password'),
-    path('dashboard/', views.dashboard, name='dashboard'),
-    path('dashboard/myinquiries', views.myinquiries, name='myinquiries'),
-    path('dashboard/inquiry1', views.inquiry1, name='inquiry1'),
-    path('dashboard/send_reply', views.send_reply, name="send_reply"),
     path('reset-password', PasswordResetView.as_view(), name='reset-password'),
     path('reset-password/done', PasswordResetDoneView.as_view(),
          name='password_reset_done'),
@@ -24,8 +20,4 @@ urlpatterns = [
          PasswordResetConfirmView.as_view(), name='password_reset_confirm'),
     path('reset-password/complete/', PasswordResetCompleteView.as_view(),
          name='password_reset_complete'),
-
-    path('fav/<int:id>/', views.add_favourite, name='add_favourite'),
-    #     path('unfav/<int:id>/', views.delete_favourite, name='delete_favourite'),
-    path('favourites/', views.favourites_list, name='favourites_list')
 ]
