23078d82840fef69722342c2edcb274fc982d630
fineobiboy <63299759+fineobiboy@users.noreply.github.com>
2021-04-26 03:39:13 +0300

    Add inquiry model

--- inquiry/models.py
+++ inquiry/models.py
@@ -7,7 +7,7 @@ class inquiry(models.Model):
     listing_id = models.IntegerField()
     name = models.CharField(max_length=255)
     email = models.EmailField(max_length=255)
-    phone = models.CharField(max_length=255)
+    phone = models.CharField(max_length=256)
     message = models.TextField(blank=True)
     contact_date = models.DateField(default=datetime.now)
     user_id = models.IntegerField(blank=True)
